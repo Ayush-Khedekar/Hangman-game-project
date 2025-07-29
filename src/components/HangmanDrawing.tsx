d27b1df4f25831c646acc9da68adedec0205097d
Ayush <ayushkhedekar07@gmail.com>
2025-07-29 16:18:16 +0530

    Smoll changes

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,25 +1,25 @@
 const HEAD = (
-  <div className="w-[15%] aspect-square rounded-full border-[1.2vw] border-black absolute top-[12%] right-[-7%]" />
+  <div className="w-[13%] aspect-square rounded-full border-[1vw] border-black absolute top-[10%] right-[-6%]" />
 );
 
 const BODY = (
-  <div className="w-[2%] h-[22%] bg-black absolute top-[23%] right-0" />
+  <div className="w-[1.8%] h-[20%] bg-black absolute top-[22%] right-0" />
 );
 
 const RIGHT_ARM = (
-  <div className="w-[22%] h-[2%] bg-black absolute top-[30%] right-[-22%] rotate-[-30deg] origin-bottom-left" />
+  <div className="w-[20%] h-[1.8%] bg-black absolute top-[28%] right-[-20%] rotate-[-30deg] origin-bottom-left" />
 );
 
 const LEFT_ARM = (
-  <div className="w-[22%] h-[2%] bg-black absolute top-[30%] right-[2%] rotate-[30deg] origin-bottom-right" />
+  <div className="w-[20%] h-[1.8%] bg-black absolute top-[28%] right-[2%] rotate-[30deg] origin-bottom-right" />
 );
 
 const RIGHT_LEG = (
-  <div className="w-[22%] h-[2%] bg-black absolute top-[42%] right-[-20%] rotate-[55deg] origin-bottom-left" />
+  <div className="w-[20%] h-[1.8%] bg-black absolute top-[40%] right-[-18%] rotate-[55deg] origin-bottom-left" />
 );
 
 const LEFT_LEG = (
-  <div className="w-[22%] h-[2%] bg-black absolute top-[42%] right-0 rotate-[-55deg] origin-bottom-right" />
+  <div className="w-[20%] h-[1.8%] bg-black absolute top-[40%] right-0 rotate-[-55deg] origin-bottom-right" />
 );
 
 const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
@@ -30,12 +30,19 @@ type HangmanDrawingProps = {
 
 const HangmanDrawing = ({ numberOfGuesses }: HangmanDrawingProps) => {
   return (
-    <div className="relative w-[60vw] sm:w-[40vw] md:w-[30vw] lg:w-[300px] aspect-[3/4] mx-auto">
+    <div className="relative w-[50vw] sm:w-[35vw] md:w-[28vw] lg:w-[260px] aspect-[3/4] mx-auto">
       {BODY_PARTS.slice(0, numberOfGuesses)}
 
+      {/* Top rope */}
       <div className="absolute top-0 right-0 h-[12%] w-[2%] bg-black" />
+
+      {/* Top beam */}
       <div className="absolute top-0 left-[40%] h-[2%] w-[60%] bg-black" />
+
+      {/* Vertical pole */}
       <div className="absolute top-0 left-[40%] h-full w-[2%] bg-black" />
+
+      {/* Base */}
       <div className="absolute bottom-0 left-0 h-[2%] w-full bg-black" />
     </div>
   );
